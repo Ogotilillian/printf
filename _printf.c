243b7d5226f3d618b75cd548127763f1a69c88c2
Ogotilillian <ogotilillian3@gmail.com>
2023-10-14 13:47:56 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,49 +1,58 @@
 #include "main.h"
 /**
  * _printf-the main function that emitates the standard output printf.
- * *format: Contains the character to be checked
+ * @format: It is a string containing format specifiers
+ * and optional text, format specifiers start with a % character and are used
+ * to specify how the additional arguments (variadic arguments) should be
+ * formatted and inserted into the string.
  * ...: This is representing the variadic funtion
  * Return: always 0 (succes).
  */
 int _printf(const char *format, ...)
 {
 	int joval_s = 0;
-va_list args_in_list;
-if (format == NULL)
-	return (-1);
-va_start(args_in_list, format);
-while (*format)
-{
-if (*format != '%')
-{		write(1, format, 1);
-joval_s++;}
-else
-{
-		format++;
-	    if (*format == '\0')
-break;
-	    if (*format == '%'){
-write(1, format, 1);
-joval_s++;}
-	else if (*format == 'c')
+	va_list args_in_list;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args_in_list, format);
+	while (*format)
 	{
-		char c = va_arg(args_in_list, int);
-write(1, &c, 1);
-joval_s++;
-}
-else if (*format == 's')
-{
-	char *str = va_arg(args_in_list, char*);
-int len_str = 0;
-	    while (str[len_str] != '\0')
-{
-len_str++;
-}
-write(1, str, len_str);
-joval_s += len_str;
-}}
-format++;
-}
-va_end(args_in_list);
-return (joval_s);
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			joval_s++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				joval_s++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args_in_list, int);
+
+				write(1, &c, 1);
+				joval_s++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args_in_list, char*);
+				int len_str = 0;
+
+				while (str[len_str] != '\0')
+					len_str++;
+				write(1, str, len_str);
+				joval_s += len_str;
+			}
+		}
+		format++;
+	}
+	va_end(args_in_list);
+	return (joval_s);
 }
